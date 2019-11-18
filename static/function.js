006d624eacb6c8095f23bb17faf4702a847b56ef
qasimmehdi <qasim_127@hotmail.com>
2019-11-18 22:37:16 +0500

    Updates at frontend
    Added ability to add username to the message. No changes at backend.

--- static/function.js
+++ static/function.js
@@ -1,40 +1,24 @@
 function ready() {
     let socket = io();
     console.log(socket);
+    let commingMessage = document.getElementById('commingMessage');
+    let username = document.getElementById('username');
 
-    document.getElementById('sendMessage').onclick = function () {
-        socket.emit('new message', document.getElementById('commingMessage').value);
+
+    document.getElementById('sendMessage').onclick = function() {
+        if (username.value.length > 0 && commingMessage.value.length > 0) {
+            socket.emit('new message', (username.value + ': ' + commingMessage.value));
+        }
     };
 
-    socket.on('new message', function (commingMessage) {
-        let node = document.createElement("H1");                 // Create a <li> node
-        let textnode = document.createTextNode(commingMessage);         // Create a text node
-        node.appendChild(textnode);                              // Append the text to <li>
+
+
+    socket.on('new message', function(commingMessage) {
+        let node = document.createElement("H3"); // Create a <li> node
+        let textnode = document.createTextNode(commingMessage); // Create a text node
+        node.appendChild(textnode); // Append the text to <li>
         document.body.appendChild(node);
-        document.getElementById('commingMessage').value = '';
+        commingMessage.value = '';
     })
 
 };
-
-/*$('#sendMessage').click(() => {
-    socket.emit('new message', $('#commingMessage').val());
-})*/
-
-/*socket.on('new message', (commingMessage) => {
-    $('#messages').append('<div class="oldmessages">' + commingMessage + '</div>');
-    $('#commingMessage').val('');
-})*/
-
-/*<div>
-<div id = 'messages'>
-  <div class="oldmessages">Hello</div>
-  <div class="oldmessages">Hey</div>
-  <div class="oldmessages">WhatsUP</div>
-  <div class="oldmessages">NotjingDOWN</div>
-</div>*/
-
-/*#newMessage {
-    position: fixed;
-    bottom: 0;
-    width: 100%;
-}*/
