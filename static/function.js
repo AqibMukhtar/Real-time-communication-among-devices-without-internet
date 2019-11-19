331a94324fba77dfba371ee7b136c19c2fb55cf2
qasimmehdi <qasim_127@hotmail.com>
2019-11-19 17:30:11 +0500

    alert username

--- static/function.js
+++ static/function.js
@@ -1,20 +1,26 @@
+var username = "";
+
 function ready() {
+    while(username.length === 0)
+    {
+        username = prompt('Enter username');
+    }
     let socket = io();
     console.log(socket);
     let commingMessage = document.getElementById('commingMessage');
-    let username = document.getElementById('username');
 
 
     document.getElementById('sendMessage').onclick = function() {
-        if (username.value.length > 0 && commingMessage.value.length > 0) {
-            socket.emit('new message', (username.value + ': ' + commingMessage.value));
+        if (commingMessage.value.length > 0) {
+            socket.emit('new message', (username + ': ' + commingMessage.value));
+            commingMessage.value = "";
         }
     };
 
 
 
     socket.on('new message', function(commingMessage) {
-        let node = document.createElement("H3"); // Create a <li> node
+        let node = document.createElement("h4"); // Create a <li> node
         let textnode = document.createTextNode(commingMessage); // Create a text node
         node.appendChild(textnode); // Append the text to <li>
         document.body.appendChild(node);
