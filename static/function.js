1fa44893331ecc7e3a3bd406643bdec200c2960d
AnusBaig <48921961+AnusBaig@users.noreply.github.com>
2020-08-12 00:08:39 +0500

    Minor UI changes

--- static/function.js
+++ static/function.js
@@ -1,30 +1,55 @@
-var username = "";
-
 function ready() {
-    while(username.length === 0)
-    {
-        username = prompt('Enter username');
-    }
     let socket = io();
-    console.log(socket);
-    let commingMessage = document.getElementById('commingMessage');
 
+    document.getElementById('sendMessage').onclick = () =>
+        emitMessage(socket,document.getElementById('commingMessage').value);
 
-    document.getElementById('sendMessage').onclick = function() {
-        if (commingMessage.value.length > 0) {
-            socket.emit('new message', (username + ': ' + commingMessage.value));
-            commingMessage.value = "";
-        }
+    document.getElementById('commingMessage').onkeydown = (e) => {
+        if(e.keyCode==13)
+        emitMessage(socket,e.target.value);
     };
 
+    listenMessage(socket);
+}
 
+function emitMessage(source,message){
+    if(message!="") source.emit('new message', message);
+} 
 
-    socket.on('new message', function(commingMessage) {
-        let node = document.createElement("h4"); // Create a <li> node
-        let textnode = document.createTextNode(commingMessage); // Create a text node
-        node.appendChild(textnode); // Append the text to <li>
-        document.body.appendChild(node);
-        commingMessage.value = '';
-    })
+function listenMessage(destination){
+    let messsagePosition='right';
 
-};
+    destination.on('new message', (commingMessage) => {
+        let node = document.createElement("div");                 // Create a <li> node
+        let textnode = document.createTextNode(commingMessage);         // Create a text node
+        node.appendChild(textnode); 
+        
+        messsagePosition = (messsagePosition=='left')?'right':'left';  
+        node.setAttribute('class','message '+messsagePosition)                           // Append the text to <li>
+        document.getElementById('conversation__area').appendChild(node);
+        document.getElementById('commingMessage').value = '';
+    });
+}
+
+/*$('#sendMessage').click(() => {
+    socket.emit('new message', $('#commingMessage').val());
+})*/
+
+/*socket.on('new message', (commingMessage) => {
+    $('#messages').append('<div class="oldmessages">' + commingMessage + '</div>');
+    $('#commingMessage').val('');
+})*/
+
+/*<div>
+<div id = 'messages'>
+  <div class="oldmessages">Hello</div>
+  <div class="oldmessages">Hey</div>
+  <div class="oldmessages">WhatsUP</div>
+  <div class="oldmessages">NotjingDOWN</div>
+</div>*/
+
+/*#newMessage {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+}*/
